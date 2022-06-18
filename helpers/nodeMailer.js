309db0dd2c63994da82158a736327518c3eb2520
CliqRight <102127461+cliqright@users.noreply.github.com>
2022-06-18 08:07:38 -0700

    Update nodeMailer.js

--- helpers/nodeMailer.js
+++ helpers/nodeMailer.js
@@ -1,6 +1,40 @@
 /*
+@copyright : ToXSL Technologies Pvt. Ltd. < www.toxsl.com >
+@author     : Shiv Charan Panjeta < shiv@toxsl.com >
+ 
+All Rights Reserved.
+Proprietary and confidential :  All information contained herein is, and remains
+the property of ToXSL Technologies Pvt. Ltd. and its partners.
+Unauthorized copying of this file, via any medium is strictly prohibited.
+*/
+
+const nodemailer = require("nodemailer");
+var FCM = require("fcm-push");
+const twilio = require("twilio");
+
 const accountSid = "AC92799ccb3423cc1daacd2585e54700d0";
 const authToken = "fff438c76de04ab0da257d7565fa2a52";
+
+const client = require("twilio")(accountSid, authToken);
+
+module.exports = {
+  sendSMS: (mobileNumber, body) => {
+    console.log("mobileNumber   : ", mobileNumber, "  body   :  ", body);
+    client.messages.create(
+      {
+        body: body,
+        to: mobileNumber,
+        from: "+13208531892",
+      },
+      (twilioErr, twilioResult) => {
+        if (twilioErr) {
+          console.log("Twillio error");
+        } else {
+          console.log("Twillio result", twilioResult);
+        }
+      }
+    );
+  },
 
   /**
    * Node Mailer :
